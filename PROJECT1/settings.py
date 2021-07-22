42acc0bdbe0195ee0018adb8833c76b2393ec785
JOHN_MUIA <muiaj267@gmail.com>
2021-07-22 15:56:21 +0300

    hhh

--- PROJECT1/settings.py
+++ PROJECT1/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-^8t0@9t%)5n%uy8!8jckzl1j(fwsthp4sn6-df^k**h_7z03ag
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['vennmuia.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['vennmuiaa.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
